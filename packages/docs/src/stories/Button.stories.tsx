c949bdd6457e5f2042b1e24f6ca5d433a947e358
Diogo <diogorocha12@live.com>
2023-05-22 21:28:13 -0300

    feat: adicionando projeto de blog ao monorepo

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,67 +1,13 @@
-import type { StoryObj, Meta } from "@storybook/react";
-import { Button, ButtonProps } from "@diroc-ui/react";
-import { ArrowRight } from "phosphor-react";
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@diroc-ui/react'
 
 export default {
-  title: "Form/Button",
+  title: 'Form/Button',
   component: Button,
   args: {
-    children: "Send",
-    variant: "primary",
-    size: "md",
-    disabled: false,
+    children: 'Button',
   },
-  argTypes: {
-    variant: {
-      options: ["primary", "secondary", "tertiary"],
-      control: { type: "inline-radio" },
-    },
-    size: {
-      options: ["sm", "md"],
-      control: { type: "inline-radio" },
-    },
-    disabled: {
-      control: { type: "boolean" },
-    },
-    onClick: { action: "clicked" },
-  },
-} as Meta<ButtonProps>;
+  argTypes: {},
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {};
-
-export const Secondary: StoryObj<ButtonProps> = {
-  args: {
-    variant: "secondary",
-    children: "Create",
-  },
-};
-
-export const Tertiary: StoryObj<ButtonProps> = {
-  args: {
-    variant: "tertiary",
-    children: "Cancel",
-  },
-};
-
-export const Small: StoryObj<ButtonProps> = {
-  args: {
-    size: "sm",
-  },
-};
-
-export const WhiteIcon: StoryObj<ButtonProps> = {
-  args: {
-    children: (
-      <>
-        Proximo Passo
-        <ArrowRight weight="bold" />
-      </>
-    ),
-  },
-};
-
-export const Disabled: StoryObj<ButtonProps> = {
-  args: {
-    disabled: true,
-  },
-};
+export const Primary: StoryObj<ButtonProps> = {}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,11 +1,1 @@
-export * from "./components/Box";
-export * from "./components/Text";
-export * from "./components/Heading";
-export * from "./components/Avatar";
-export * from "./components/Button";
-export * from "./components/Checkbox";
-export * from "./components/TextArea";
-export * from "./components/TextInput";
-export * from "./components/MultiStep";
-
-export * from "./styles";
+export * from './components/Button'
